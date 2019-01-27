bc49fab0d49cdf71b7b3975c5b4db95a06439164
Ayush Raj <raj.ayush012@gmail.com>
2019-01-27 16:27:48 +0530

    First commit of the project

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    fs.appendFileSync('server.log',log + '\n');
+    console.log(log); 
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+   // res.send('<h1>Hello World!</h1>');
+    // res.send({
+    //     name: 'Ayush',
+    //     likes: [
+    //         'loving Samriddhi',
+    //         'Eating her too'
+    //     ]
+    // });
+    res.render('home.hbs',{
+        title: 'Welcome Home!',
+        body: 'Here is the body',
+    
+    })
+});
+
+app.get('/about', (req,res)=>{
+    //res.send('about page');
+    res.render('about.hbs',{
+        title: 'About paga!',
+        
+    })
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Failed to load the page'
+    });
+});
+app.listen(3000, () => {
+    console.log('Server is up!');
+});
